7ef53daf0c6cad0e8a44f5c7e9fceaac5d0cd392
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-03-13 12:13:11 +0700

    Ysl

--- compile.sh
+++ compile.sh
@@ -3,17 +3,29 @@
 # Compile script kernel
 # Copyright (C) 2024-2025 Rve.
 
+git clone --depth=1 https://github.com/Mi-Eldarion/msm-4.19 kernel
+
+cd kernel
+
+git clone https://github.com/Mi-Eldarion/kernel_devicetree-ysl arm64/boot/dts/vendor/qcom/ysl
+
+git clone https://github.com/Mi-Eldarion/kernel_techpack_ysl techpack/mi8953
+
+wget https://github.com/Rv-Project/RvClang/releases/download/20.1.0/RvClang-20.1.0-bolt-pgo-full_lto.tar.gz
+
+tar -xvzf RvClang-20.1.0-bolt-pgo-full_lto.tar.gz
+
 CLANGDIR="/home/rve/RvClang"
 
 rm out/compile.log
 mkdir -p out
 
-export KBUILD_BUILD_USER=Rve
-export KBUILD_BUILD_HOST=RvProject
+export KBUILD_BUILD_USER=RennAlt
+export KBUILD_BUILD_HOST=Mi-Eldarion
 export USE_CCACHE=1
 export PATH="$CLANGDIR/bin:$PATH"
 
-make O=out ARCH=arm64 rvkernel-alioth_defconfig
+make O=out ARCH=arm64 vendor/msm8953-perf_defconfig
 
 rve () {
 make -j$(nproc --all) O=out LLVM=1 LLVM_IAS=1 \
